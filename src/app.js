60270f16df990f5c6b3f38ff6b48344b137d96f6
chrisleimacau <chrislei.macau@gmail.com>
2022-04-30 21:43:40 +0800

    Create API call to searchMovie

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+//imdbAPI 65f90792
+const API_URL = 'https://www.omdbapi.com?apikey=65f90792';
 
 const App = () => {
+
+    const searchMovie = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovie('Spiderman');
+    }, []);
+
     return (
         <>
             <h1>My Movie App</h1>
